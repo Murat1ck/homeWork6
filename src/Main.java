96278025bbcb4fd08c326b8fa437d5f0839ef167
Murat1ck <muratoff101088@gmail.com>
2022-10-05 11:37:52 +0300

    Задание №7

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++)
+        for (int i = 1; i <= 512; i = i * 2)
             System.out.println(i);
     }
 }
